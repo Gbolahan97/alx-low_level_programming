abab3b1887c4787027d06eea398bd42aa3607ac2
Gbolahan97 <omogbolawon@gmail.com>
2022-09-08 18:27:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  *
  *  *main - Entry point
  *
  *   *My hello, printf task
  *
- *    *Return: return 1 after program execution
+ *    *Return: return 0 after program execution
  *
  *     */
 int main(void)
