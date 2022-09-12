5e3fac8a6f1d3d211f0455151b6f12207c3828e5
Gbolahan97 <omogbolawon@gmail.com>
2022-09-12 14:04:11 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry pont
+*
+* Return: Always 0 (success/correct)
+*/
+int main(void)
+{
+char (letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != '0' && letter != 'q')
+		putchar(letter);
+}
+putchar('\n');
+return (0)
+}	
